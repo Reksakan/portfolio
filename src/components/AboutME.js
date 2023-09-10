f8efab1a836596753a60f99f40c920b78622659b
reksakan <reksakan@reksa-debian.debian>
2023-09-10 00:22:59 -0400

    Firebase updating process check via github

--- src/components/AboutME.js
+++ src/components/AboutME.js
@@ -12,7 +12,7 @@ const AboutME = () => {
         </div>
         <div className="col-lg-5 col-xm-12">
           <h1 className="about-heading">about me</h1>
-          <p>Hi! My name is Asker. I'm Front-end Web Developer. Technologies I use are ReactJS, NodeJS, Express.</p>
+          <p>Hi! My name is AskeR. I'm Front-end Web Developer. Technologies I use are ReactJS, NodeJS, Express.</p>
           <p>After recognizing the positibe impact of a successfully integrated web app on a day-to-day business activities on my previous company, I decided to pivot my career into Web Development. I'm eager to leverage the coding skills I have gained at Brainstation with the problem solving and communication skills I have gained through my working experience to create new products and technologies.</p>
         </div>
       </div> 
